a6c377fe79cb329d89eeb68b4e9ac34634e53073
Jordan <jnwong116@gmail.com>
2021-11-23 22:03:24 -0500

    Alpha release submit

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -2,26 +2,123 @@
 
 const basketballData = {
     sport: 'basketball',
-    team1: 'Toronto Raptors',
-    team2: 'New York Knicks',
+    team1: 'New York Knicks',
+    team2: 'Toronto Raptors',
+    team1color: 'blue',
+    team2color: 'red',
     team1starters: {
-        pg: 'Kemba Walker',
-        sg: 'Evan Fournier',
-        sf: 'RJ Barrett',
-        pf: 'Julius Randle',
-        c: 'Mitchell Robinson'
+        pg: {
+            name: 'Kemba Walker',
+            number: 8
+        },
+        sg: {
+            name: 'Evan Fournier',
+            number: 13
+        },
+        sf: {
+            name: 'RJ Barrett',
+            number: 9
+        },
+        pf: {
+            name: 'Julius Randle',
+            number: 30
+        },
+        c: {
+            name: 'Mitchell Robinson',
+            number: 23
+        }
     },
     team2starters: {
-        pg: 'Fred VanVleet',
-        sg: 'Gary Trent Jr.',
-        sf: 'OG Anunoby',
-        pf: 'Pascal Siakam',
-        c: 'Precious Achiuwa'
+        pg: {
+            name: 'Fred VanVleet',
+            number: 23
+        },
+        sg: {
+            name: 'Gary Trent Jr.',
+            number: 33
+        },
+        sf: {
+            name: 'OG Anunoby',
+            number: 3
+        },
+        pf: {
+            name: 'Pascal Siakam',
+            number: 43
+        },
+        c: {
+            name: 'Precious Achiuwa',
+            number: 5
+        }
     },
-    team1bench: ['Derrick Rose', 'Immanuel Quickley', 'Alec Burks', 'Obi Toppin', 'Taj Gibson', 'Nerlens Noel', 'Quentin Grimes', 'Jericho Sims'],
-    team2bench: ['Scottie Barnes', 'Goran Dragic', 'Yuta Watanabe', 'Chris Boucher', 'Khem Birch', 'Malachi Flynn']
+    team1bench: [{name: 'Derrick Rose', number: 4}, {name: 'Immanuel Quickley', number: 5}, {name: 'Alec Burks', number: 18}, {name: 'Obi Toppin', number: 1}, {name: 'Taj Gibson', number: 67}, {name: 'Quentin Grimes', number: 6}, {name: 'Nerlens Noel', number: 3}, {name: 'Jericho Sims', number: 45}],
+    team2bench: [{name: 'Scottie Barnes', number: 4}, {name: 'Goran Dragic', number: 1}, {name: 'Yuta Watanabe', number: 18}, {name: 'Chris Boucher', number: 25}, {name: 'Khem Birch', number: 24}, {name: 'Malachi Flynn', number: 22}],
+    team1stats: {
+        score: 104,
+        fg: '35/76',
+        fgPer: 46.1,
+        threePtrs: '16/38',
+        threePtrPer: 42.1,
+        ft: '18/24',
+        ftPer: 75,
+        rebounds: 41,
+        offReb: 7,
+        assists: 20,
+        block: 4,
+        steal: 3, 
+        turnover: 16,
+        fouls: 13
+    },
+    team2stats: {
+        score: 113,
+        fg: '43/99',
+        fgPer: 43.4,
+        threePtrs: '14/42',
+        threePtrPer: 33.3,
+        ft: '13/14',
+        ftPer: 92.9,
+        rebounds: 48,
+        offReb: 13,
+        assists: 24,
+        block: 4,
+        steal: 12, 
+        turnover: 8,
+        fouls: 20
+    },
+    team1PlayerStats: [
+        {name: 'RJ Barrett', MinP: 39, Reb: 6, Ast: 0, Pts: 27}, {name: 'Julius Randle', MinP: 37, Reb: 9, Ast: 5, Pts: 22}, {name: 'Mitchell Robinson', MinP: 28, Reb: 12, Ast: 3, Pts: 6},
+    ],
+    highlights: [
+        {currScore: '104 - 101',
+        play:  "Kemba double team, pass to RJ Barrett, RJ open three"}
+    ]
 }
 
 const sportTracker = new sportTraker(basketballData)
-sportTracker.makeCourt()
-sportTracker.logData()
+
+function showLineup() {
+    clearPage();
+    sportTracker.makeCourt(0.5)
+    sportTracker.displayPlayers();
+}
+
+function showStats() {
+    clearPage();
+    sportTracker.showStats();
+}
+
+function clearPage() {
+    const court = document.getElementsByClassName('court')[0];
+    if (court !== undefined) {
+        document.body.removeChild(court);
+    }
+    const table = document.getElementsByClassName('statsTable')[0];
+    if (table !== undefined) {
+        document.body.removeChild(table);
+    }
+    
+}
+
+function showHighlight(num) {
+    sportTracker.playAnimation(num);
+}
+
--- pub/js/sportTrak.js
+++ pub/js/sportTrak.js
@@ -1,6 +1,12 @@
 "use strict";
 const log = console.log
 
+const animationList = [
+    {
+        sport: 'basketball',
+        animations: ['double team', 'pass to', 'open three', 'dunk', 'fadeaway', 'step back three']
+    }
+]
 
 function sportTraker(data) {
     this.sport = data.sport
@@ -10,20 +16,36 @@ function sportTraker(data) {
     this.team2starters = data.team2starters
     this.team1bench = data.team1bench
     this.team2bench = data.team2bench
+    this.team1color = data.team1color
+    this.team2color = data.team2color
 
-    // this.stats = data.stats
+    // Court and player circles generated
+    this.court = ''
+    this.team1players = []
+    this.team2players = []
 
-    // this.highlights = data.highlights
+    this.team1stats = data.team1stats
+    this.team2stats = data.team2stats
+
+    this.table = '';
+    this.team1PlayerStats = data.team1PlayerStats
+    // this.team2PlayerStats = data.team2PlayerStats
+
+    this.highlights = data.highlights
+    this.scale = 0;
     
 }
 
 sportTraker.prototype = {
 
     // Creates the court for each game
-    makeCourt: function() {
+    // Scales the court down to a size, otherwise it will take up whole page
+    makeCourt: function(scale) {
+        this.scale = scale;
+        
         const court = document.createElement('section')
         court.className = 'court'
-
+        
         const main = document.createElement('main')
 
         for (let i = 0; i < 2; i++) {
@@ -36,7 +58,7 @@ sportTraker.prototype = {
             const freeThrow = document.createElement('div')
             freeThrow.className = 'free-throw'
 
-            const key = document.createElement('key')
+            const key = document.createElement('div')
             key.className = 'key'
 
             freeThrow.appendChild(key)
@@ -46,8 +68,98 @@ sportTraker.prototype = {
         }
 
         court.appendChild(main)
+        court.style = 'transform: scale(' + scale + ');'
         document.body.appendChild(court)
+        this.court = court;
+    },
 
+    displayPlayers: function() {
+        const team1 = this.team1;
+        const team2 = this.team2;
+
+        const team1starters = this.team1starters;
+        const team2starters = this.team2starters;
+
+        const team1bench = this.team1bench;
+        const team2bench = this.team2bench;
+
+        const court = this.court;
+
+        // Generates the player circles
+        Object.keys(team1starters).forEach(key => {
+            const player = document.createElement('span');
+            player.classList.add('player');
+            player.classList.add(key);
+            player.classList.add('team1');
+            player.style = 'background-color: ' + this.team1color + ';'
+
+            const number = document.createElement('span');
+            number.innerHTML = team1starters[key].number;
+            number.className = 'number';
+
+            const name = document.createElement('span');
+            name.innerHTML = team1starters[key].name;
+            name.className = 'playerName';
+
+            player.appendChild(name);
+            player.appendChild(number);
+            court.appendChild(player);
+
+            this.team1players.push(player);
+        })
+
+        // Generates team 2 player circles
+        Object.keys(team2starters).forEach(key => {
+            const player = document.createElement('span');
+            player.classList.add('player');
+            player.classList.add(key);
+            player.classList.add('team2');
+            player.style = 'background-color: ' + this.team2color + ';'
+
+            const number = document.createElement('span');
+            number.innerHTML = team2starters[key].number;
+            number.className = 'number';
+
+            const name = document.createElement('span');
+            name.innerHTML = team2starters[key].name;
+            name.className = 'playerName';
+
+            player.appendChild(name);
+            player.appendChild(number);
+            court.appendChild(player);
+
+            this.team2players.push(player)
+        })
+
+        // Creates the benches
+
+        const benchTeam1 = document.createElement('div');
+        const benchHeader1 = document.createElement('h2');
+        benchHeader1.innerHTML = team1;
+        
+        benchTeam1.appendChild(benchHeader1);
+
+        Object.keys(team1bench).forEach(key => {
+            benchTeam1.innerHTML += team1bench[key].number + ', '  + team1bench[key].name + '<br />';
+        })
+
+        benchTeam1.classList.add('team1bench');
+        benchTeam1.classList.add('bench');
+        court.appendChild(benchTeam1);
+
+        const benchTeam2 = document.createElement('div');
+        const benchHeader2 = document.createElement('h2');
+        benchHeader2.innerHTML = team2;
+        
+        benchTeam2.appendChild(benchHeader2);
+
+        Object.keys(team2bench).forEach(key => {
+            benchTeam2.innerHTML += team2bench[key].number + ', '  + team2bench[key].name + '<br />';
+        })
+
+        benchTeam2.classList.add('team2bench');
+        benchTeam2.classList.add('bench');
+        court.appendChild(benchTeam2);
     },
 
     logData: function() {
@@ -59,5 +171,221 @@ sportTraker.prototype = {
         log(this.team2bench)
         log(this.team2starters)
         // log(this.stats)
+    },
+
+    showStats: function() {
+        const table = document.createElement('table');
+        const tableHeaders = document.createElement('tr');
+        const team1 = document.createElement('th');
+        const statText = document.createElement('th');
+        const team2 = document.createElement('th');
+
+        team1.innerHTML = this.team1;
+        team2.innerHTML = this.team2;
+        statText.innerHTML = 'TEAM STATS';
+        table.className = 'statsTable';
+
+        tableHeaders.appendChild(team1);
+        tableHeaders.appendChild(statText);
+        tableHeaders.appendChild(team2);
+        table.appendChild(tableHeaders);
+
+        Object.keys(this.team1stats).forEach(key => {
+            const row = document.createElement('tr');
+            const team1stat = document.createElement('td');
+            const statText = document.createElement('td');
+            const team2stat = document.createElement('td');
+
+            console.log(key);
+
+            switch (key) {
+                case 'score':
+                    statText.innerHTML = "Final Score";
+                    break;
+                case 'fg':
+                    statText.innerHTML = "Field Goals";
+                    break;
+                case 'fgPer':
+                    statText.innerHTML = "Field Goal Percentage";
+                    break;
+                case 'threePtrs':
+                    statText.innerHTML = "Three Pointers";
+                    break;
+                case 'threePtrPer':
+                    statText.innerHTML = "Three Pointer Percentage";
+                    break;
+                case 'ft':
+                    statText.innerHTML = "Free Throws";
+                    break;
+                case 'ftPer': 
+                    statText.innerHTML = "Free Throw Percentage";
+                    break;
+                case 'rebounds':
+                    statText.innerHTML = "Total Rebounds";
+                    break;
+                case 'offReb':
+                    statText.innerHTML = "Offensive Rebounds";
+                    break
+                case 'assists':
+                    statText.innerHTML = "Assists"
+                    break;
+                case 'block':
+                    statText.innerHTML = "Blocks";
+                    break
+                case 'steal':
+                    statText.innerHTML = "Steals";
+                    break
+                case 'turnover': 
+                    statText.innerHTML = "Turnovers";
+                    break
+                case 'fouls':
+                    statText.innerHTML = "Fouls";
+                    break
+            }
+
+            team1stat.innerHTML = this.team1stats[key];
+            team2stat.innerHTML = this.team2stats[key];
+
+            row.appendChild(team1stat);
+            row.appendChild(statText);
+            row.appendChild(team2stat);
+            table.appendChild(row);
+
+        })
+        
+        document.body.appendChild(table);
+        this.table = table;
+    },
+
+    playAnimation: function(animationNum) {
+        const court = this.court;
+        log(this.team1players)
+        const highlight = this.highlights[animationNum];
+
+        let animations = [];
+        animationList.forEach(sport => {
+            if (sport.sport == this.sport) {
+                animations = sport.animations;
+            }
+        });
+        
+        const currScore = highlight.currScore;
+        
+        const players = []
+        const playerAnimations = []
+        const animationSteps = highlight.play.split(",");
+        animationSteps.forEach(play => {
+            animations.forEach(animation => {
+                if (play.includes(animation)) {
+                    playerAnimations.push(animation);
+                    players.push(play.replace(animation, ""));
+                }
+            });
+        });
+
+        // Checks which team is attacking
+        let attackingTeam = '';
+        let attackingTeamNum = 0;
+        let defendingTeam = '';
+        Object.keys(this.team1starters).forEach(player => {
+            if (this.team1starters[player].name.includes(players[0])) {
+                attackingTeam = this.team1players;
+                attackingTeamNum = 1;
+                defendingTeam = this.team2players;
+            }
+        });
+        if (attackingTeam === '') {
+            attackingTeam = this.team2players;
+            attackingTeamNum = 2;
+            defendingTeam = this.team1players;
+        }
+
+        // Moves attacking team up
+        for (let i = 0; i < attackingTeam.length; i++) {
+            const player = attackingTeam[i]
+            player.classList.add('attacking' + attackingTeamNum + Object.keys(this.team1starters)[i]);
+        }
+
+        const playersInPlay = []
+        // Links players in play to the player object
+        for (let i = 0; i < players.length; i++) {
+            const player = players[i].trim()
+            for (let j = 0; j < Object.keys(this.team1starters).length; j++) {
+                const team1player = this.team1starters[Object.keys(this.team1starters)[j]].name;
+                if (team1player.includes(player)) {
+                    playersInPlay.push({
+                        player: this.team1players[j],
+                        defender: j})
+                }
+            }
+        }
+
+        // Creates basketball
+        const basketball = createBasketball()
+        playersInPlay[0].player.appendChild(basketball)
+
+        // Animates the play
+        let previousAnimation = '';
+        let previousPlayer = playersInPlay[0].player;
+        let playerWithBall = playersInPlay[0].player;
+        for (let i = 0; i < playerAnimations.length; i++) {
+            setTimeout(function() {
+                let playerInAnimation = playersInPlay[i].player;
+                let defendingPlayer = defendingTeam[playersInPlay[i].defender];
+                
+                // Do previous animation first
+                if (previousAnimation !== '') {
+                    switch (previousAnimation) {
+                        case 'double team':
+                            defendingPlayer.style.transform = "translate(" + (-previousPlayer.offsetLeft + 130)  + "px, " + (-previousPlayer.offsetTop + 140) + "px)";
+                            break;
+                    }
+                }
+
+                switch (playerAnimations[i]) {
+                    case 'double team':
+                        previousAnimation = 'double team';
+                        previousPlayer = playersInPlay[i].player;
+                        playerWithBall = playersInPlay[i].player;
+                        break;
+                    case 'pass to':
+                        playerWithBall = playersInPlay[i].player;
+                        const rect = playerWithBall.getBoundingClientRect();
+                        // court.appendChild(newBasketball)
+                        // log(rect.left)
+                        // newBasketball.style = "left: " + -rect.left + "px; top: " + -rect.top + "px;";
+                        previousPlayer.removeChild(basketball);
+                        playerWithBall.appendChild(basketball)
+                        break;
+                    case 'open three':
+                        playerWithBall = '';
+                        basketball.classList.add('shot');
+                        break;
+                }
+            }, 1000)
+
+            
+        }
+
+        
+
+        'double team', 'pass to', 'open three', 'dunk', 'fadeaway', 'step back three'
+
     }
+}
+
+function createBasketball() {
+    const basketball = document.createElement('div');
+    basketball.className = 'basketball'
+
+    const ball = document.createElement('div');
+    ball.className = 'ball'
+
+    const lines = document.createElement('div')
+    lines.className = 'lines'
+
+    ball.appendChild(lines)
+    basketball.appendChild(ball);
+
+    return basketball;
 }
